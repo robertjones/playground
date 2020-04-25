9dd297f445afba241561f5b01c6b66e85a71e46d
Rob Jones <robert.buckingham.jones@gmail.com>
2020-04-25 15:31:05 +0100

    v1

--- js.js
+++ js.js
@@ -1,9 +1,38 @@
 var root = document.body
 
-m.render(root, [
-m("h1", "1 + 2 ="),
-m("table", 
-  Array(2).fill(m("tr",
-    Array(5).fill(m("td",  {class: "p-1"}, m("button", {class: "bg-blue-500 hover:bg-blue-700 text-white font-bold text-2xl py-3 px-5 rounded"}, "2")))
-  ))
-)])
+const probgen = () => {
+  let ans = Math.floor(Math.random() * 10) + 1
+  let a = Math.floor(Math.random() * (ans + 1))
+  return {a: a, b: ans - a, ans: ans}
+}
+
+var prob = probgen()
+var msg = ""
+var dis = []
+
+const numclick = (num, ans) => {
+  if (num == ans) {
+    prob = probgen()
+    msg = msg == "☹️" ? "😃" : msg + "😃"
+    dis = []
+  } else {
+    msg = "☹️"
+    dis.push(num)
+  }
+}
+
+var Game = {
+  view: () => {
+    return m("div", [
+      m("p", {class: "text-5xl font-bold my-4"}, prob.a + " + " + prob.b + " = __"),
+      m("table", {class: "self-center mx-auto my-4"},  
+        [0,1].map(row => m("tr",
+          [0,1,2,3,4].map(col => m("td",  {class: "p-1"}, m("button", {onclick: () => numclick(row*5 + col + 1, prob.ans), class: dis.includes(row*5 + col + 1) ? "bg-red-500 hover:bg-red-700 text-white font-bold text-3xl w-16 h-12 rounded" : "bg-blue-500 hover:bg-blue-700 text-white font-bold text-3xl w-16 h-12 rounded"}, dis.includes(row*5 + col + 1) ? "X" : row*5 + col + 1)))
+        ))
+      ),
+      m("p", {class: "text-6xl"}, msg)
+    ])
+  }
+}
+
+m.mount(root, Game)
